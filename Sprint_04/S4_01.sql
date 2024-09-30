6c27645572a5b61098f92eb1ebe5229626e2df11
leocareer <leo.ph.career@gmail.com>
2024-09-30 19:52:19 +0200

    preparing the repository

---
+++ Sprint_04/S4_01.sql
@@ -0,0 +1,11 @@
+-- Level 1 Exercise 1
+-- Perform a subquery that displays all users with more than 30 transactions using at least 2 tables.
+
+-- Level 1 Exercise 2
+-- Show the average amount by IBAN of the credit cards in the Donec Ltd company, use at least 2 tables.
+
+-- Level 2 Exercise 1
+-- Create a new table that reflects the credit card status based on whether the last three transactions were declined and generate the following query: how many cards are active?
+
+-- Level 3 Exercise 1
+-- Create a table with which we can join the data from the new products.csv file with the created database, taking into account that from transaction you have product_ids. Generate the following query: we need to know the number of times each product has been sold.
