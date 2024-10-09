84bfed251194063c5621c6fadcd10903d33496d3
leocareer <leo.ph.career@gmail.com>
2024-10-09 11:40:01 +0200

    sprint 4: data loading is done

--- Sprint_04/db_creating.sql
+++ Sprint_04/db_creating.sql
@@ -3,15 +3,15 @@ CREATE DATABASE commerce;
 USE commerce;
 
 -- Сreating tables
-CREATE TABLE credit_card (
+CREATE TABLE credit_cards (
 	id VARCHAR(20) PRIMARY KEY,
     user_id INT,
     iban VARCHAR(34),
-    pan VARBINARY(128), 
-    pin VARBINARY(64),
-    cvv VARBINARY(64),
-    track1 VARBINARY(128),
-    track2 VARBINARY(128),
+    pan VARCHAR(20), 
+    pin VARCHAR(4),
+    cvv VARCHAR(3),
+    track1 VARCHAR(50),
+    track2 VARCHAR(50),
     expiring_date DATE
 );
 
@@ -39,11 +39,11 @@ CREATE TABLE users (
     surname VARCHAR(20),
     phone VARCHAR(15),
     email VARCHAR(100),
-    birth_date DATE,
+    birth_date VARCHAR(15),
     country VARCHAR(50),
     city VARCHAR(50),
     postal_code VARCHAR(10),
-    adress VARCHAR(100)
+    address VARCHAR(100)
 );
 
 CREATE TABLE transactions (
@@ -55,9 +55,9 @@ CREATE TABLE transactions (
     declined TINYINT,
     product_ids VARCHAR(200),
     user_id INT,
-    lat DECIMAL(10,10),
-    longitude DECIMAL(10,10),
-    FOREIGN KEY (card_id) REFERENCES credit_card(id),
+    lat DECIMAL(15,10),
+    longitude DECIMAL(15,10),
+    FOREIGN KEY (card_id) REFERENCES credit_cards(id),
     FOREIGN KEY (business_id) REFERENCES companies(company_id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
@@ -77,4 +77,97 @@ INTO TABLE companies
 FIELDS TERMINATED BY ',' 
 IGNORE 1 LINES;
 
-SHOW VARIABLES LIKE 'secure_file_priv';
+LOAD DATA INFILE '/tmp/credit_cards.csv'
+INTO TABLE credit_cards
+FIELDS TERMINATED BY ','
+IGNORE 1 LINES
+(@id, @user_id, @iban, @pan, @pin, @cvv, @track1, @track2, @expiring_date)
+SET 
+	id = TRIM(@id), 
+    user_id = TRIM(@user_id),
+    iban = TRIM(@iban),
+    pan = TRIM(@pan),
+    pin = TRIM(@pin),
+    cvv = TRIM(@cvv),
+    track1 = TRIM(@track1),
+    track2 = TRIM(@track2),
+    expiring_date = STR_TO_DATE(@expiring_date, '%m/%d/%y');
+
+LOAD DATA INFILE '/tmp/products.csv'
+INTO TABLE products
+FIELDS TERMINATED BY ',' 
+IGNORE 1 LINES;
+
+LOAD DATA INFILE '/tmp/users_usa.csv'
+INTO TABLE users
+FIELDS TERMINATED BY ',' 
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES
+	(@id, @name, @surname, @phone, @email, @birth_date, @country, @city, @postal_code, @address)
+SET
+    id = @id,
+    name = @name,
+    surname = @surname,
+    phone = @phone,
+    email = @email,
+    birth_date = STR_TO_DATE(@birth_date, '%b %d, %Y'),
+    country = @country,
+    city = @city,
+    postal_code = @postal_code,
+    address = @address;
+    
+LOAD DATA INFILE '/tmp/users_ca.csv'
+INTO TABLE users
+FIELDS TERMINATED BY ',' 
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES
+	(@id, @name, @surname, @phone, @email, @birth_date, @country, @city, @postal_code, @address)
+SET
+    id = @id,
+    name = @name,
+    surname = @surname,
+    phone = @phone,
+    email = @email,
+    birth_date = STR_TO_DATE(@birth_date, '%b %d, %Y'),
+    country = @country,
+    city = @city,
+    postal_code = @postal_code,
+    address = @address;
+    
+LOAD DATA INFILE '/tmp/users_uk.csv'
+INTO TABLE users
+FIELDS TERMINATED BY ',' 
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES
+	(@id, @name, @surname, @phone, @email, @birth_date, @country, @city, @postal_code, @address)
+SET
+    id = @id,
+    name = @name,
+    surname = @surname,
+    phone = @phone,
+    email = @email,
+    birth_date = STR_TO_DATE(@birth_date, '%b %d, %Y'),
+    country = @country,
+    city = @city,
+    postal_code = @postal_code,
+    address = @address;
+
+LOAD DATA INFILE '/tmp/transactions.csv'
+INTO TABLE transactions
+FIELDS TERMINATED BY ';' 
+IGNORE 1 LINES
+(@id, @card_id,	@business_id, @timestamp, @amount, @declined, @product_ids, @user_id, @lat, @longitude)
+SET
+	id = TRIM(@id),
+    card_id = TRIM(@card_id),
+    business_id = TRIM(@business_id),
+    timestamp = TRIM(@timestamp),
+    amount = TRIM(@amount),
+    declined = TRIM(@declined),
+    product_ids = TRIM(@product_ids),
+    user_id = TRIM(@user_id),
+    lat = TRIM(@lat),
+    longitude = TRIM(@longitude);
